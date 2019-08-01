b7fec6bba94bf08f67e6577044d36b77e2def64c
Jochen Roth <j.roth@zeroseven.de>
2019-08-01 08:20:25 +0200

    Fix rvm in test

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ echo "Ruby:                  " `ruby --version` || exit 1
 echo "Ruby Gems:             " `gem --version` || exit 1
 echo "Node:                  " `node --version` || exit 1
 echo "NVM:                   " `. /root/.nvm/nvm.sh && nvm --version` || exit 1
-echo "RVM:                   " `rvm -v` || exit 1
+echo "RVM:                   " `/bin/bash -l -c "rvm -v"` || exit 1
 echo "NPM:                   " `npm --version` || exit 1
 echo "Yarn:                  " `yarn --version` || exit 1
 echo "Gitlab Package Updater:" `gitlab-package-updater.rb -v` || exit 1
