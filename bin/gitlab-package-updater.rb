8e5646f7fd8b128886dc396c7862423aa86dd60b
Jochen <rothjochen@gmail.com>
2019-08-11 02:33:28 +0200

    Fix version output triggered commits and actions

--- bin/gitlab-package-updater.rb
+++ bin/gitlab-package-updater.rb
@@ -14,41 +14,39 @@ require './app/git_action'
 options = CliOptions.get
 
 unless options[:version]
-  exit
-end
+  git = GitAction.new(options)
+  git.create_branch
+  git.checkout
 
-git = GitAction.new(options)
-git.create_branch
-git.checkout
+  markdown = ''
 
-markdown = ''
+  if (File.exist?("#{options[:repo]}/composer.lock"))
+    composer = ComposerUpdater.new(options)
+    composer.install
+    outdated = composer.outdated.to_s
+    markdown << outdated unless outdated.nil?
+    composer.update
+  end
 
-if (File.exist?("#{options[:repo]}/composer.lock"))
-  composer = ComposerUpdater.new(options)
-  composer.install
-  outdated = composer.outdated.to_s
-  markdown << outdated unless outdated.nil?
-  composer.update
-end
+  if (File.exist?("#{options[:repo]}/yarn.lock"))
+    yarn = YarnUpdater.new(options)
+    outdated = yarn.outdated
+    markdown << outdated unless outdated.nil?
+    yarn.update
+  end
 
-if (File.exist?("#{options[:repo]}/yarn.lock"))
-  yarn = YarnUpdater.new(options)
-  outdated = yarn.outdated
-  markdown << outdated unless outdated.nil?
-  yarn.update
-end
+  if (File.exist?("#{options[:repo]}/package-lock.json"))
+    npm = NpmUpdater.new(options)
+    outdated = npm.outdated
+    markdown << outdated unless outdated.nil?
+    npm.update
+  end
 
-if (File.exist?("#{options[:repo]}/package-lock.json"))
-  npm = NpmUpdater.new(options)
-  outdated = npm.outdated
-  markdown << outdated unless outdated.nil?
-  npm.update
-end
+  if git.files_changed.count > 0
+    git.add
+    git.commit
+    git.push
 
-if git.files_changed.count > 0
-  git.add
-  git.commit
-  git.push
-
-  ApiRequest.new(options, markdown).merge_request
+    ApiRequest.new(options, markdown).merge_request
+  end
 end
--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "PHP Default:           " `php -v` || exit 1
+echo "PHP 7.1:               " `/usr/bin/php7.1 -v` || exit 1
+echo "PHP 7.2:               " `/usr/bin/php7.2 -v` || exit 1
+echo "PHP 7.3:               " `/usr/bin/php7.3 -v` || exit 1
 echo "Composer:              " `composer --version` || exit 1
 echo "Ruby:                  " `ruby --version` || exit 1
 echo "Ruby Gems:             " `gem --version` || exit 1
